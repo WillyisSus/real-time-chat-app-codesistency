84cf4be42ce7c685537601dc4459d15bd675fc44
Dingkemon <116236317+WillyisSus@users.noreply.github.com>
2025-07-06 15:05:14 +0700

    add message APIs (no realtime yet)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from"./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js"
 dotenv.config()
 const app = express();
 app.use(express.json())
 app.use("/api/auth", authRoutes)
-
+app.use("/api/messages", messageRoutes)
 const PORT = process.env.PORT
 app.listen(PORT, ()=> {
     console.log("Server is runner on port " + PORT)
